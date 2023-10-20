9516b6fa56cbd8997a77d2be6eaf6f4ee0e07a39
Kevin Hintz <khintz34@gmail.com>
2023-10-19 22:39:09 -0500

    google direbase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,46 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.scss";
+import { ref as databaseRef, onValue } from "firebase/database";
+import { db } from "../assets/firebase";
+import { useState, useEffect } from "react";
+import { getDownloadURL, getStorage, ref } from "firebase/storage";
 
 export default function Home() {
+  useEffect(() => {
+    getUserData();
+  }, []);
+
+  async function getUserData() {
+    let holdingArray: Array<string> = [];
+    const boardRef = databaseRef(db, "FoodList/");
+    let displayArray: Array<any> = [];
+    onValue(
+      boardRef,
+      (snapshot) => {
+        snapshot.forEach((childSnapShot) => {
+          const childKey = childSnapShot.key;
+          const childData = childSnapShot.val();
+          console.log(childData);
+          let obj = {
+            name: childData.name,
+            category: childData.category,
+          };
+          addData(obj);
+        });
+      },
+      {
+        onlyOnce: false,
+      }
+    );
+
+    function addData(obj: any) {
+      // displayArray.push(obj);
+      // setMemoryList([...displayArray]);
+      console.log(obj);
+    }
+  }
+
   return <main className={styles.main}>test</main>;
 }
---
+++ src/assets/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBuofcSxEVVETSdVPwfrwX1DmqzLPq41vU",
+  authDomain: "grocery-list-54ddf.firebaseapp.com",
+  databaseURL: "https://grocery-list-54ddf-default-rtdb.firebaseio.com",
+  projectId: "grocery-list-54ddf",
+  storageBucket: "grocery-list-54ddf.appspot.com",
+  messagingSenderId: "844158994963",
+  appId: "1:844158994963:web:78ed629b7fa3c00d0ecde6",
+  measurementId: "G-BJVBNNNCCM",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+const analytics =
+  app.name && typeof window !== "undefined" ? getAnalytics(app) : null;
+
+export const db = getDatabase();
+
+export const storage = getStorage();
