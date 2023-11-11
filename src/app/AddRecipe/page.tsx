af003d22997d880af55f648e4494fc03a03cb7a5
Kevin Hintz <khintz34@gmail.com>
2023-11-10 22:44:24 -0600

    add labels

--- src/app/AddRecipe/page.tsx
+++ src/app/AddRecipe/page.tsx
@@ -1,14 +1,57 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./page.module.scss";
+import { CategoryList } from "../../assets/CategoryList";
+import { getDatabase, push, ref, set, remove } from "firebase/database";
 
 export default function AddRecipe() {
+  const [category, setCategory] = useState("Baby Food");
+
+  function writeUserData(e) {
+    e.preventDefault();
+
+    const database = getDatabase();
+    set(ref(database, "FoodList/" + name), {
+      Name: name,
+      Category: e.target.value,
+    }).catch((error) => {
+      console.log("Uploaded Unsuccessfully... try again. ");
+    });
+  }
+
   return (
     <main className={styles.main}>
       <form action="">
-        <div>
-          <label htmlFor="name">Recipe Name</label>
+        <div className={styles.inputContainer}>
+          <label htmlFor="name" className={styles.label}>
+            Recipe Name
+          </label>
           <input type="text" name="name" id="name" />
+        </div>
+        <div className={styles.inputContainer}>
+          <label htmlFor="categorySel" className={styles.label}>
+            Category
+          </label>
+          <select
+            name="categorySel"
+            onChange={(e) => {
+              writeUserData(e);
+              //   add();
+              setCategory(e.target.value);
+            }}
+            defaultValue={"Baby Food"}
+            className={styles.select}
+          >
+            {CategoryList.map((option, index) => (
+              <option
+                key={index}
+                value={option}
+                className={`${styles.option} `}
+              >
+                {option}
+              </option>
+            ))}
+          </select>
         </div>
       </form>
     </main>
