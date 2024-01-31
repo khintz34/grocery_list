240902f597ed04d1514435ce3f51ad6c3ddefd53
Kevin Hintz <khintz34@gmail.com>
2024-01-30 20:50:08 -0600

    poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 import { AuthContextProvider } from "../contexts/authContext";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ weight: ["100", "400", "800"], subsets: ["latin"] });
 
 export const metadata = {
   title: "Grocery List",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={poppins.className}>
         <AuthContextProvider>{children}</AuthContextProvider>
       </body>
     </html>
